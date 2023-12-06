1c2e6ceab741ad59bb093e165966d0e4abf68e13
MoriahYen <moriahyen@gmail.com>
2023-12-06 17:50:15 +0800

    [Code] Required middleware "Morgan".

--- 4-natours/after-section-06/app.js
+++ 4-natours/after-section-06/app.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
 
 const app = express();
+
+/* 1. MIDDLEWARE*/
+app.use(morgan('dev'))
+
 app.use(express.json())  // [Moriah] 要有這個assign才能merge
 
 // [Moriah] define middleware
@@ -16,6 +21,8 @@ app.use((req, res, next) => {
 })
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+/* 2. ROUTE HANDLERS*/
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime)
@@ -98,15 +105,49 @@ const deleteTour = (req, res) => {
   })
 }
 
-// app.get('/api/v1/tours', getAllTours)
-// app.get('/api/v1/tours/:id', getTour)
-// app.post('/api/v1/tours', createTour)
-// app.patch('/api/v1/tours/:id', updateTour)
-// app.delete('/api/v1/tours/:id', deleteTour)
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  })
+}
 
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  })
+}
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  })
+}
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  })
+}
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  })
+}
+
+/* 3. ROUTES*/
 app.route('/api/v1/tours').get(getAllTours).post(createTour)
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
+app.route('/api/v1/users').get(getAllUsers).post(createUser)
+app.route('/api/v1/users:id').get(getUser).patch(updateUser).delete(deleteUser)
+
+/* 4. START SERVER*/
 const port = 3000
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
