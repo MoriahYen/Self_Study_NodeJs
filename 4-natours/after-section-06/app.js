f197d56bb12e2dc263d4b48291dcedcbe52ccd95
MoriahYen <moriahyen@gmail.com>
2023-12-06 17:15:17 +0800

    [Code] Modified code. Solved the error which "assign" merged failed.

--- 4-natours/after-section-06/app.js
+++ 4-natours/after-section-06/app.js
@@ -2,12 +2,26 @@ const fs = require('fs')
 const express = require('express');
 
 const app = express();
+app.use(express.json())  // [Moriah] 要有這個assign才能merge
+
+// [Moriah] define middleware
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!')
+  next()
+})
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime)
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
@@ -41,7 +55,6 @@ const createTour = (req, res) => {
   //  console.log(req.body)
 
   const newId = tours[tours.length - 1].id + 1
-  // [Moriah] Can't merge, why?
   const newTour = Object.assign({ id: newId }, req.body)  // [Moriah]: assign>>將兩個obj合成一個新的obj
 
   tours.push(newTour)
