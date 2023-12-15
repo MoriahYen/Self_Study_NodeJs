b912b82b4a49ed5732f2771f6ebdea82acb6282f
MoriahYen <moriahyen@gmail.com>
2023-12-15 22:28:06 +0800

    [Code] Use Mongoose.

--- 4-natours/after-section-06/server.js
+++ 4-natours/after-section-06/server.js
@@ -1,9 +1,37 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv');
-dotenv.config({ path: './after-section-06/config.env' });
-
 const app = require('./app');
 
-console.log(process.env)
+dotenv.config({ path: './after-section-06/config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+  // [Moriah] 處理棄用警告的選項
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log('DB connection successful!'))
+
+// [Moriah] 指定類型
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],  // [Moriah] validater
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+
+// [Moriah] model name/params 用大寫
+const Tour = mongoose.model('Tour', tourSchema)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
