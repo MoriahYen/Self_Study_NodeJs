cdd6e68d1d6c64bd74da96416e6d3b9a90208f1e
MoriahYen <moriahyen@gmail.com>
2023-12-21 18:00:42 +0800

    [Code] Import/delete dev-data from json file.

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,22 +1,76 @@
 const mongoose = require('mongoose')
 
-const tourSchema = new mongoose.Schema(
-  {
+const tourSchema = new mongoose.Schema({
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
+      trim: true
     },
-    rating: {
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      // enum: {
+      //   values: ['easy', 'medium', 'difficult'],
+      //   message: 'Difficulty is either: easy, medium, difficult'
+      // }
+    },
+    ratingsAverage: {
       type: Number,
       default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
     },
     price: {
       type: Number,
       required: [true, 'A tour must have a price']
     },
-  }
-)
+    priceDiscount: {
+      type: Number,
+      // validate: {
+      //   validator: function(val) {
+      //     // this only points to current doc on NEW document creation
+      //     return val < this.price;
+      //   },
+      //   message: 'Discount price ({VALUE}) should be below regular price'
+      // }
+    },
+    summary: {
+      type: String,
+      // [Moriah] trim only work on string, 會刪除開頭和結尾的空格
+      trim: true,
+      required: [true, 'A tour must have a description']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      //select: false
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
+})
 
 const Tour = mongoose.model('Tour', tourSchema)
 
