6dac756acf4208da350754c95469a5c67e1bc544
MoriahYen <moriahyen@gmail.com>
2023-12-22 10:53:22 +0800

    [Code] Pagination.

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -36,6 +36,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v')
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+    // page=3&limit=10, 1-10: page1
+    query = query.skip(skip).limit(limit)
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments()  // [Moriah] return a promise
+      if (skip >= numTours) throw new Error('This page does not exit')
+    }
+
     // EXECUTE QUERY
     const tours =  await query
 
