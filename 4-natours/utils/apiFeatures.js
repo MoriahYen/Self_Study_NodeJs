5bee8e919a87bff33e518c82ab209dc22887da2d
MoriahYen <moriahyen@gmail.com>
2023-12-28 17:36:21 +0800

    [Code] Fixed error.(Can't show on postman)

--- 4-natours/utils/apiFeatures.js
+++ 4-natours/utils/apiFeatures.js
@@ -37,7 +37,7 @@ class APIFeatures {
   limitFields() {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ')
-      this.query = this.query.select('fields')
+      this.query = this.query.select(fields)
     } else {
       this.query = this.query.select('-__v')
     }
