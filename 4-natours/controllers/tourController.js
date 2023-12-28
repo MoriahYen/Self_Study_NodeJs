43b9ab94672bed90b7b46f6feb954821cd1cd08f
MoriahYen <moriahyen@gmail.com>
2023-12-28 22:29:47 +0800

    [Code] Catching Errors in Async Functions.

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel')
 const APIFeatures = require('./../utils/apiFeatures')
+const catchAsync = require('./../utils/catchAsync')
 
 exports.aliasTopTours = (req, res, next) => {
   // limit=5&sort=-ratingsAverage,price
@@ -9,202 +10,153 @@ exports.aliasTopTours = (req, res, next) => {
   next()
 }
 
-exports.getAllTours = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate()
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate()
 
-    const tours = await features.query
+  const tours = await features.query
 
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tours
-      }
-        })
-      } catch (err) {
-        res.status(404).json({
-          status: 'fail',
-          message: err
-    })
-  }
-}
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+})
 
-exports.getTour = async (req, res) => {
-  try {
-    const tour = await Tour.findById(req.params.id)
-    // Tour.findOne({_id: req.params.id})
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id)
+  // Tour.findOne({_id: req.params.id})
 
-    res.status(200).json({
-      status: 'success',
-      message: {
-        tour
-      }
-    })
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-}
+  res.status(200).json({
+    status: 'success',
+    message: {
+      tour
+    }
+  })
+})
 
-exports.createTour = async (req, res) => {
-  try {
-    // const newTour = new Tour({})
-    // newTour.save()
-    // 不同於Model.prototype.save()
+exports.createTour = catchAsync(async (req, res, next) => {
+  //const newTour = new Tour({})
+  // newTour.save()
+  // 不同於Model.prototype.save()
 
-    const newTour = await Tour.create(req.body)
+  const newTour = await Tour.create(req.body)
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    })
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-}
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour
+    }
+  })
+})
 
-exports.updateTour = async (req, res) => {
-  try{
-    // [Moriah] https://mongoosejs.com/docs/queries.html
-      const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, 
-      runValidators: true
-    })
+exports.updateTour = catchAsync(async (req, res, next) => {
+  // [Moriah] https://mongoosejs.com/docs/queries.html
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour
-      }
-    })
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-}
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+})
 
-exports.deleteTour = async (req, res) => {
-  try{
-    // [Moriah] 不用儲存，因為是delete
-    await Tour.findOneAndDelete(req.params.id)
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  // [Moriah] 不用儲存，因為是delete
+  await Tour.findOneAndDelete(req.params.id)
 
-    res.status(204).json({
-      status: 'success',
-      data: null
-    })
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-}
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})
 
 // [Moriah] Aggregation pipeline
 // https://www.mongodb.com/docs/atlas/atlas-sp/stream-aggregation/
-exports.getTourStats = async(req, res) => {
-  try {
-    const stats = await Tour.aggregate([
-      {
-        $match: {ratingsAverage: {$gte: 4.5}}
-      },
-      {
-        $group: {
-          _id:{ $toUpper: '$difficulty' },
-          numTours: { $sum: 1 },
-          numRatings: { $sum: '$ratingsQuantity' },
-          avgRating: {$avg: '$ratingsAverage'},
-          avgPrice: {$avg: '$price'},
-          minPrice: {$min: '$price'},
-          maxPrice: {$max: '$price'}
-        }
-      },
-      {
-        $sort: { avgPrice: 1 }  // [Moriah] 1: assending
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } }
+    },
+    {
+      $group: {
+        _id: { $toUpper: '$difficulty' },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' }
       }
-      // {
-      //   $match: { _id: { $ne: 'EASY' } }
-      // }
-    ])
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats
-      }
-    })
-  } catch(err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-}
+    },
+    {
+      $sort: { avgPrice: 1 } // [Moriah] 1: assending
+    }
+    // {
+    //   $match: { _id: { $ne: 'EASY' } }
+    // }
+  ])
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats
+    }
+  })
+})
 
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const year = req.params.year * 1
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1
 
-    const plan =  await Tour.aggregate([
-      {
-        // [Moriah] unwind: 從檔案中解構一個數組字段，再為數組的每個元素輸出一個文檔
-        // ex: The Forest Hiker有三個日期，所以分成三個文檔
-        $unwind: '$startDates'
-      },
-      {
-        // [Moriah] match用來選擇文檔
-        $match: {
-          startDates: {
-            $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-31`)
-          }
+  const plan = await Tour.aggregate([
+    {
+      // [Moriah] unwind: 從檔案中解構一個數組字段，再為數組的每個元素輸出一個文檔
+      // ex: The Forest Hiker有三個日期，所以分成三個文檔
+      $unwind: '$startDates'
+    },
+    {
+      // [Moriah] match用來選擇文檔
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`)
         }
-      },
-      {
-        $group: {
-          _id: {$month: '$startDates'},
-          numTourStarts: {$sum: 1},
-          tours: {$push: '$name'}
-        }
-      },
-      {
-        $addFields: {month: '$_id'}
-      },
-      {
-        $project: {_id: 0}
-      },
-      {
-        $sort: {numTourStarts: -1}
-      },
-      {
-        $limit: 12
       }
-    ])
-    res.status(200).json({
-      status: 'success',
-      data: {
-        plan
+    },
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: '$name' }
       }
-    })
-  } catch(err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-}
+    },
+    {
+      $addFields: { month: '$_id' }
+    },
+    {
+      $project: { _id: 0 }
+    },
+    {
+      $sort: { numTourStarts: -1 }
+    },
+    {
+      $limit: 12
+    }
+  ])
+  res.status(200).json({
+    status: 'success',
+    data: {
+      plan
+    }
+  })
+})
