6a387011942c9f356a8f4d086cc9e9f6c36ac1a3
MoriahYen <moriahyen@gmail.com>
2024-01-02 17:38:06 +0800

    [Code] Handling Invalid Database IDs.(Can't work on Postman)

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = err => {
+  const message = 'Invalid ${err.path}: ${err.value}.'
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -15,7 +22,7 @@ const sendErrorProd = (err, res) => {
       message: err.message
     })
 
-  // Programming or other unknown error: don't leak error details
+    // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
     console.error('ERROR', err)
@@ -35,6 +42,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res)
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res)
+    let error = { ...err }
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error)
+
+    sendErrorProd(error, res)
   }
 }
