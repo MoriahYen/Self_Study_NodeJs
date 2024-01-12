57e388448909faf5b8ab670b0aae82e4c1ede4cb
MoriahYen <moriahyen@gmail.com>
2024-01-12 11:10:24 +0800

    [Code] Errors Outside Express: Unhandled Rejections.

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -6,18 +6,26 @@ const app = require('./app')
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_PASSWORD
 )
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
+    useFindAndModify: false
   })
   .then(() => console.log('DB connection successful!'))
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`)
+})
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message)
+  console.log('UNHANDLER REJECTION! Shutting down')
+  server.close(() => {
+    process.exit(1)
+  })
 })
