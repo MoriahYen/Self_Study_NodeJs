6c864519026504da692391fc57df4a3bcd28baec
MoriahYen <moriahyen@gmail.com>
2024-01-16 11:07:45 +0800

    [Code] Protecting Tour Routes.("passwordChangedAt"can't work on Postman)

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password!'],
-    minlength: 8
+    minlength: 8,
+    select: false // [Moriah] No show on postman
   },
   passwordConfirm: {
     type: String,
@@ -30,7 +31,8 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Password are not the same'
     }
-  }
+  },
+  passwordChangedAt: Date
 })
 
 // [Moriah] pre save middleware
@@ -38,6 +40,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
   // Only run this function if password was actually modified
   if (!this.isModified('password')) return next()
+
   // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12)
 
@@ -45,6 +48,25 @@ userSchema.pre('save', async function(next) {
   this.passwordConfirm = undefined
   next()
 })
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword)
+}
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    )
+    console.log(changedTimestamp, JWTTimestamp)
+    return JWTTimestamp < changedTimestamp
+  }
+  return false
+}
 
 const User = mongoose.model('User', userSchema)
 
