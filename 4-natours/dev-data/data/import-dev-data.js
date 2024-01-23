24f3a9519fc0063c9e638b25e314017e80a524fd
MoriahYen <moriahyen@gmail.com>
2024-01-23 20:36:24 +0800

    [Code] Modelling Locations (Geospatial Data).

--- 4-natours/dev-data/data/import-dev-data.js
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -7,26 +7,26 @@ dotenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_PASSWORD
 )
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
+    useFindAndModify: false
   })
   .then(() => console.log('DB connection successful!'))
 
 // READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 // IMPRT DATA INTO DATABASE
 const importdData = async () => {
   try {
     await Tour.create(tours)
     console.log('Data successfully loaded!')
-  } catch(err) {
+  } catch (err) {
     console.log(err)
   }
   process.exit()
@@ -37,7 +37,7 @@ const deleteData = async () => {
   try {
     await Tour.deleteMany()
     console.log('Data successfully deleted!')
-  } catch(err) {
+  } catch (err) {
     console.log(err)
   }
   process.exit()
--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
-// const validator = require('validator')
+//const validator = require('validator')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -78,8 +78,39 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
-},
+    },
+    // [Moriah] Embadded Obj
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
 {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
