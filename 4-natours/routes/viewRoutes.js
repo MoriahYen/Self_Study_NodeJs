880ec78710ae79c6a28a8bb9e6d2f5c23fe14f14
MoriahYen <moriahyen@gmail.com>
2024-05-22 16:59:44 +0800

    [Code] Building the Login Screen

--- 4-natours/routes/viewRoutes.js
+++ 4-natours/routes/viewRoutes.js
@@ -9,7 +9,10 @@ router.get(
   '/tour/:slug',
   /*authController.isLoggedIn,*/ viewsController.getTour
 );
-// router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get(
+  '/login',
+  /*authController.isLoggedIn,*/ viewsController.getLoginForm
+);
 // router.get('/me', authController.protect, viewsController.getAccount);
 
 // router.post(
