e01207c0610497e875ae56891723444d168e797d
MoriahYen <moriahyen@gmail.com>
2024-07-03 22:22:06 +0800

    [Code] Sending Password Reset Emails.

--- 4-natours/utils/email.js
+++ 4-natours/utils/email.js
@@ -58,10 +58,10 @@ module.exports = class Email {
     await this.send('welcome', 'Welcome to the Natours Family!');
   }
 
-  // async sendPasswordReset() {
-  //   await this.send(
-  //     'passwordReset',
-  //     'Your password reset token (valid for only 10 minutes)'
-  //   );
-  // }
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
 };
